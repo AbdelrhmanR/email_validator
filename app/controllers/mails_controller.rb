dd53cd3c065ec57721bb44aed1a69338d2c7af7c
abdelrhman <abdelrhmanelheely@gmailcom>
2019-03-20 05:39:23 +0200

    Assign mails controller

--- app/controllers/mails_controller.rb
+++ app/controllers/mails_controller.rb
@@ -1,12 +1,8 @@
 require 'httparty'
 class MailsController < ApplicationController
-
-
-
   def index
   	url = 'http://apilayer.net/api/check?access_key=d3380e037f72bf7c7d2dbeafd94862de&email=abdelrhman.elheely@gmail.com'
     response = HTTParty.get(url)
   	render json: response.body
-  
   end
 end
