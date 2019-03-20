61dd2017176f88f15176098537d90583f829c45f
abdelrhman <abdelrhmanelheely@gmailcom>
2019-03-21 00:55:08 +0200

    Assign the right endpoint url

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 Rails version: 5.1.6.2
 Ruby  version: 2.3.3
 * bundle
-
 http://localhost:3000/mails/index    #end point
+>> You can chick in postman >> Key :  email   ,   Value  : test.me@gmail.com
--- app/controllers/mails_controller.rb
+++ app/controllers/mails_controller.rb
@@ -1,7 +1,7 @@
-require 'httparty'
+
 class MailsController < ApplicationController
   def index
-  	url = 'http://apilayer.net/api/check?access_key=d3380e037f72bf7c7d2dbeafd94862de&email=test.me@gmail.com'
+  	url = "http://apilayer.net/api/check?access_key=d3380e037f72bf7c7d2dbeafd94862de&email=#{params[:email]}"
     response = HTTParty.get(url)
   	render json: response.body
   end
